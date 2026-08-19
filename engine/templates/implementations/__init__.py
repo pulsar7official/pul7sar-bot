d3485e48449705a1d4271756972b16a59d516b8f
pulsar7official <pulsar7.official@gmail.com>
2026-08-19 14:27:47 +0300

    Add files via upload

---
+++ engine/templates/implementations/__init__.py
@@ -0,0 +1,6 @@
+"""Concrete template implementations for the PUL7SAR Visual Engine."""
+
+from engine.templates.implementations.default import DefaultTemplate
+from engine.templates.implementations.news import NewsTemplate
+
+__all__ = ["DefaultTemplate", "NewsTemplate"]
