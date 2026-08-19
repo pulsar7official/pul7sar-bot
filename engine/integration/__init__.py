444097164d3c4c2ee017518ca82ab4a8c989a77f
pulsar7official <pulsar7.official@gmail.com>
2026-08-19 14:50:25 +0300

    Add files via upload

---
+++ engine/integration/__init__.py
@@ -0,0 +1,5 @@
+"""Production integration helpers for the PUL7SAR Visual Engine."""
+
+from engine.integration.article_adapter import render_article_with_engine
+
+__all__ = ["render_article_with_engine"]
