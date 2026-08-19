2be245ea5e64a9efd0a9b6df0680d688d029d2da
pulsar7official <pulsar7.official@gmail.com>
2026-08-19 14:50:12 +0300

    Delete engine/integration/__init__.py

--- engine/integration/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Production integration helpers for the PUL7SAR Visual Engine."""
-
-from engine.integration.article_adapter import render_article_with_engine
-
-__all__ = ["render_article_with_engine"]
